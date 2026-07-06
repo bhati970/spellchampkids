14612fc8b4c7f141185fe9943edc1899d0ce9a3a
bhati970 <bhativikramsinh@gmail.com>
2026-07-06 18:11:29 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,33 @@
-const quizData=[];
+const quizData = [
+
+{
+question:"Correct Spelling?",
+options:["Aple","Apple","Appel","Appl"],
+answer:1
+},
+
+{
+question:"Correct Spelling?",
+options:["Ball","Bal","Baal","Bll"],
+answer:0
+},
+
+{
+question:"Correct Spelling?",
+options:["Cat","Kat","Catt","Ct"],
+answer:0
+},
+
+{
+question:"Correct Spelling?",
+options:["Dog","Dogg","Doog","Doge"],
+answer:0
+},
+
+{
+question:"Correct Spelling?",
+options:["Fish","Fis","Fesh","Fishh"],
+answer:0
+}
+
+];
