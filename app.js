0474d7f0419b5e461a15f1dc1e5dccc89c1c42c3
bhati970 <bhativikramsinh@gmail.com>
2026-07-06 18:10:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('v2');
+// Spell Champ Kids v3
+
+function openQuiz() {
+    window.location.href = "quiz.html";
+}
+
+function comingSoon(name) {
+    alert(name + " Module Coming Soon!");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const buttons = document.querySelectorAll(".menu button");
+
+    if (buttons.length >= 5) {
+
+        buttons[0].onclick = openQuiz;
+
+        buttons[1].onclick = () => comingSoon("Learn");
+
+        buttons[2].onclick = () => comingSoon("Practice");
+
+        buttons[3].onclick = () => comingSoon("Progress");
+
+        buttons[4].onclick = () => comingSoon("Settings");
+    }
+
+});
